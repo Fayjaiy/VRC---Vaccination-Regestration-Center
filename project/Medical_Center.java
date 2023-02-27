0a50cda442eccc44788167335f4e7c78747cd3f8
Fatma Abujaid <102144973+Fayjaiy@users.noreply.github.com>
2023-02-27 23:07:06 +0300

    Add files via upload

---
+++ project/Medical_Center.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project;
+
+/**
+ *
+ * @author shahadaleissa
+ */
+public class Medical_Center {
+
+    public Medical_Center() {
+    }
+
+    public Medical_Center(String medId, String centerName, String location) {
+        this.medId = medId;
+        this.centerName = centerName;
+        this.location = location;
+    }
+
+    
+    
+    private String medId;
+    private String centerName;
+    private String location;
+  
+    
+}
